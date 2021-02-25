116bf1d540055e7f47e0773cf2b5f8c4c2336fc7
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-25 09:08:12 +0700

    first

---
+++ src/main/java/id/ten/projectreactor/basicreactive/BasicReactiveApplication.java
@@ -0,0 +1,13 @@
+package id.ten.projectreactor.basicreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ten/projectreactor/basicreactive/BasicReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.projectreactor.basicreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
